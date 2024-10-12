9071645568cc91c87b0fc1f62e381ff685f5c6c3
Michel Marion <kangkm19@gmail.com>
2024-10-12 21:35:43 +0100

    Began app preview

--- src/components/taskbar/taskbar-components/ActiveApps.jsx
+++ src/components/taskbar/taskbar-components/ActiveApps.jsx
@@ -7,33 +7,46 @@ const matchIcon = (appName) => {
     return taskbarIcon ? taskbarIcon.icon : null;
 };
 
-const renderTaskbarApp = (app, isActive, handleRemoveTaskbarApp, setActiveApp) => {
+const renderTaskbarApp = (app, isActive, hoveredApp, handleRemoveTaskbarApp, setActiveApp, setHoveredApp) => {
     const appIcon = matchIcon(app)
 
     return (
+        <>
         <nav 
             className={`active-apps-app ${isActive ? "active" : ""}`}
             onClick={() => {
                 handleRemoveTaskbarApp(app);
                 setActiveApp(app);
             }}
+            onMouseOver={() => setHoveredApp(app)}
+            // The below does not work at the moment
+            onMouseLeave={() => setActiveApp(null)}
         >
             {appIcon && <img src={appIcon}  alt={app} />}
             <h4>{app}</h4>
         </nav>
+        <nav className={`active-apps-app--preview ${hoveredApp ? "hovered" : ""}`}>
+            <div>
+                <h4>I am a preview</h4>
+            </div>
+        </nav>
+        </>
     )
 }
 
 const ActiveApps = ({ taskbarApps, handleRemoveTaskbarApp}) => {
     const [activeApp, setActiveApp] = useState(null);
+    const [hoveredApp, setHoveredApp] = useState(null);
 
     return (
         <div className="active-apps">
             {taskbarApps.slice(0, 5).map((app, index) => {
                 const isActive = activeApp === app;
+                const isHovered = hoveredApp === app;
+
                 return (
                     <div key={index}>
-                        {renderTaskbarApp(app, isActive, handleRemoveTaskbarApp, setActiveApp)}
+                        {renderTaskbarApp(app, isActive, isHovered, handleRemoveTaskbarApp, setActiveApp, setHoveredApp)}
                     </div>
                 );
             })}
