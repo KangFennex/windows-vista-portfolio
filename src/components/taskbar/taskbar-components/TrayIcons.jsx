bb059fe1c012de9b03959c348852083012a4e5ab
Michel Marion <kangkm19@gmail.com>
2024-10-14 22:29:22 +0100

    Changed background and adapt tray icons length to screen size

--- src/components/taskbar/taskbar-components/TrayIcons.jsx
+++ src/components/taskbar/taskbar-components/TrayIcons.jsx
@@ -1,3 +1,4 @@
+import calcWindowSize from "../../utils/calcWindowSize";
 import "./TrayIcons.scss";
 import { trayIconsList } from "./taskbarIcons";
 
@@ -14,9 +15,17 @@ const renderTrayIcons = (icon) => {
 }
 
 const TrayIcons = () => {
+    const { width } = calcWindowSize();
+
+    const getVisibleIconsRange = () => {
+        if (width <= 750) return 2;
+        if (width <= 1000) return 5;
+        return 6
+    }
+
     return (
         <div className="tray-icons">
-            {trayIconsList.map((icon, index) => (
+            {trayIconsList.slice(0,getVisibleIconsRange()).map((icon, index) => (
                 <span key={index}>
                     {renderTrayIcons(icon)}
                 </span>
