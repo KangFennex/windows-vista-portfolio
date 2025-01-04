c7b1f390fdd8d3204e68eda28b5eb929a302b3b5
Michel Marion <kangkm19@gmail.com>
2025-01-04 22:50:35 +0000

    Added more external config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-draggable'],
+      external: ['react-draggable', 'react-transition-group', 'CSSTransition', 'TransitionGroup', 'uuid'],
     },
   },
 })
